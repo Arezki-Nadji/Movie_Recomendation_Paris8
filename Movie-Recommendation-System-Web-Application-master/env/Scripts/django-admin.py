226172f77b25b258e0f7cc3181196dd765941ce4
AREZKI <arezki.m.nadji96@gmail.com>
2022-12-06 19:04:38 +0100

    work

---
+++ Movie-Recommendation-System-Web-Application-master/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\arezk\Desktop\Etude\M2_Paris_8\tch\Movie-Recommendation-System-Web-Application-master\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
